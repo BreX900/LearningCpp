bba099971f68d90771afc6336d3d64ac0f9d5821
BreX900 <brexmaster@gmail.com>
2018-10-23 15:59:17 +0200

    3.1 Commit - Vector

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -4,13 +4,17 @@
 
 #include "vector.h"
 
-Vector::Vector(): v(nullptr), size(0) {}
+Vector::Vector(): v(nullptr), s(0) {}
 
-Vector::Vector(const Vector & vector): size(vector.size), v(vector.copy()) {}
+Vector::Vector(const Vector & vector): s(vector.s), v(vector.copy()) {}
+
+Vector::~Vector() {
+    if (v) delete v;
+}
 
 int *Vector::copy() const {
-    int* b = !size ? nullptr : new int[size];
-    for (int i=0; i<size; i++) {
+    int* b = !s ? nullptr : new int[s];
+    for (int i=0; i<s; i++) {
         b[i] = v[i];
     }
     return b;
@@ -20,13 +24,44 @@ Vector &Vector::operator=(const Vector &vector) {
     if (this != &vector) {
         if (v) delete[] v;
         v = vector.copy();
-        size = vector.size;
+        s = vector.s;
     }
     return *this;
 }
 
-Vector::~Vector() {
-    if (v) delete v;
+Vector Vector::operator+(const Vector &vector) const {
+    Vector w;
+    int x = s + vector.s;
+    if (x) {   //[*this.empty()||vector.empty()]  x>0
+        w.v = new int[x];
+        for (unsigned int i = 0; i < s; i++) {
+            w.v[i] = v[i];
+        }
+        for (unsigned int i = 0; i < s; i++) {
+            w.v[s + i] = vector.v[i];
+        }
+    }
+    return w;
+}
+
+bool Vector::operator==(const Vector & vector) const {
+    if (s != vector.s) return false;
+    for (unsigned int i=0; i<s; i++) {
+        if (v[i] != vector.v[i]) return false;
+    }
+    return true;
+}
+
+int &Vector::operator[](unsigned int i) const {
+    return v[i];
+}
+
+bool Vector::empty() const {   return !s;   }
+
+unsigned int Vector::size() const { return s;    }
+
+void Vector::append(const Vector &vector) {
+    *this = *this + vector;
 }
 
 
--- vector/vector.h
+++ vector/vector.h
@@ -4,15 +4,30 @@
 
 #pragma once
 
+#include <ostream>
+
 class Vector {
 public:
     Vector();
     Vector(const Vector&);
-    Vector&operator=(const Vector& vector);
     ~Vector();
+    Vector&operator=(const Vector& vector);
+    Vector operator+(const Vector&) const;
+    bool operator==(const Vector&) const;
+    int& operator[](unsigned int i) const;
+    unsigned int size() const;
+    bool empty() const;
+    void append(const Vector &);
 private:
     int* v;
-    unsigned int size;
+    unsigned int s;
     int* copy() const;
 
 };
+
+std::ostream& operator<<(std::ostream& os, const Vector& vector) {
+    for (int i=0; i<vector.size(); i++) {
+        os << vector[i] << " ";
+    }
+    return os;
+}
