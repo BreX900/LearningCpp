c5693c934a9f2c630c7d106b107a1be33710e09b
BreX900 <brexmaster@gmail.com>
2018-11-06 16:17:02 +0100

    3.1 Commit - Vector

--- classeTipizzate/threeTree.cpp
+++ classeTipizzate/threeTree.cpp
@@ -5,23 +5,54 @@
 #include "ThreeTree.h"
 
 
-static ThreeTree::Nodo::Nodo* copy(ThreeTree::Nodo::Nodo* r){
-    if (!r) return nullptr;
-    return new Nodo(r->info, copy(r->sx), copy(r->cx), copy(r->dx))
-}
-
-static void ThreeTree::destruction(Nodo* r) {
+template<class T>
+void ThreeTree<T>::destruction(Nodo* r) {
     if (r) {
-        destruction(r->sx)
-        destruction(r->cx)
-        destruction(r->dx)
+        destruction(r->sx);
+        destruction(r->cx);
+        destruction(r->dx);
         delete r;
     }
 }
-
-bool ThreeTree::operator=(const ThreeTree& t) const {
+template<class T>
+bool ThreeTree<T>::operator=(const ThreeTree<T>& t) const {
     if (this!= &t) {
         destruction(t);
-        root = copy(t.root)
+        root = copy(t.root);
     }
 }
+
+template<class T>
+void ThreeTree<T>::put(const T &t) {
+    root = new Nodo(t, root, copy(root), copy(root));
+
+}
+
+template<class T>
+bool ThreeTree<T>::search(const T &t) const {
+    return search(root, t);
+}
+
+template<class T>
+bool ThreeTree<T>::search(Nodo *r, const T &t) {
+    if (!r) return false;
+    //albero non vuoto
+    return r->info == t || search(r->sx) || search(r->cx) || search(r->dx);
+}
+template <class T>
+bool ThreeTree<T>::equals(Nodo* r1, Nodo* r2) {
+    if (!r1 && !r2) return true;
+    if (!r1 || !r2) return false;
+    return  r1->info == r2->info && equals(r1->sx, r2->sx) && equals(r1->cx, r2->cx) && equals(r1->dx, r2->dx);
+}
+
+template<class T>
+bool ThreeTree<T>::operator==(const T &t) {
+    return equals(root, t.root);
+}
+
+template<class T>
+ThreeTree::Nodo *ThreeTree<T>::Nodo::copy(ThreeTree::Nodo *r) {
+    return nullptr;
+}
+
--- classeTipizzate/threeTree.h
+++ classeTipizzate/threeTree.h
@@ -3,6 +3,7 @@
 //
 
 #pragma once
+#include <iostream>
 template <class T>
 class ThreeTree;
 
@@ -17,18 +18,22 @@ private:
     public:
         T info;
         Nodo *sx, *cx, *dx;
-        Nodo(const T& t , nodo* s=0, nodo* c=0, nodo* d=0): info(t), nx(s), cx(c), dx>(d) {}
+        Nodo(const T& t , Nodo* s=0, Nodo* c=0, Nodo* d=0): info(t), sx(s), cx(c), dx(d) {}
+        static Nodo* copy(Nodo* r);
 
+        static bool search (Nodo* r, const T& t);
     };
     Nodo* root;
-    static Nodo* copy(Nodo* r);
     static void destruction(Nodo* r);
+
 public:
     ThreeTree();
-    ThreeTree(const ThreeTree& t);
-    ThreeTree&operator=(const ThreeTree& t);
+    ThreeTree<T>(const ThreeTree& t);
+    ThreeTree& operator=(const T& t);
+    bool operator==(const T& t) const;
     ~ThreeTree();
     void put(const T& t);
     bool search(const T& t) const ;
     bool  operator=(const ThreeTree&) const ;
 };
+
